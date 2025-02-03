ab184d32dd8ea769181d07cf3d806308736d6728
renanklein <renankleinshow@gmail.com>
2025-02-03 09:43:29 -0300

    Fixes struct

--- src/value.rs
+++ src/value.rs
@@ -1,20 +1,19 @@
 use std::{ops::{Add, Mul}};
 
+
 struct Value {
     data: f64,
     children: Vec<Option<Box<Value>>>,
     label: String,
     op: String,
-    grad: f64,
-    backward: Box<dyn FnMut() -> ()>
-}
+    grad: f64
+ }
 
 impl Value {
     fn new (data: f64) -> Self {
         Value {
             data,
             children: vec![],
-            backward: Box::new(|| {}),
             grad: 0.0,
             label: String::new(),
             op: String::new()
@@ -26,12 +25,11 @@ impl Value {
         let t = (f64::exp(2.0*x) - 1.0)/(f64::exp(2.0*x) + 1.0);
         let mut out =  Value::new(self.data);
 
-        let backward = || {
-
-        };
-
         out
 
+    }
+    fn backward_add(out_grad: f64) -> f64 {
+        1.0 * out_grad
     }
 }
 
@@ -39,19 +37,17 @@ impl Add for Value{
     fn add(mut self, mut rhs: Self) -> Self::Output {
 
         let mut out = Value::new(self.data + rhs.data);
+        
+
         out.children = vec![Some(Box::new(self)), Some(Box::new(rhs))];
         out.op = String::from("+");
-        let backward = || {
-            self.grad = 1.0 * out.grad;
-            rhs.data = 1.0 * out.grad;
-        };
-
-        out.backward = Box::new(backward);
-
 
         out
 
+
     }
+
+
 
     type Output = Self;
 }
@@ -59,7 +55,15 @@ impl Add for Value{
 impl Mul for Value {
     type Output = Self;
 
-    fn mul(self, rhs: Self) -> Self::Output {
-        Self {data: self.data * rhs.data, children: vec![Some(Box::new(self)), Some(Box::new(rhs))], label: String::new()}
+    fn mul(mut self, mut rhs: Self) -> Self::Output {
+            let mut out = Value::new(self.data * rhs.data);
+
+        out.children = vec![Some(Box::new(self)), Some(Box::new(rhs))];
+        out.op = String::from("*");
+        out.label = String::new();
+
+        out
+
+
     }
 }
